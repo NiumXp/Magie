c237fa1d167a5855e9e5fab8956f781aa36ef563
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-10-26 09:27:21 -0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="Magie",
+      author="NiumXp",
+      url="https://github.com/NiumXp/Magie",
+      version="0.1.0",
+      packages=["magie"],
+      license="MIT",
+      description="A Python lib to help program with PyGame.",
+      python_requires=">=3.7.x"
+)
